69d67600902af734aaa2b83604f08cd722bde0d4
Nguyen Phi Hung <nphung2042004@gmail.com>
2026-01-29 16:35:33 +0700

    Commit 2: Tao Middleware kiem tra tuoi va Session

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
     <li><a href="/banco/8">Xem bàn cờ vua (8x8)</a></li>
     <li><a href="/test-link">Thử nghiệm Route Name</a></li>
     <li><a href="{{ route('login') }}">Đăng nhập hệ thống</a></li>
+    
     <li><a href="/signin">Đăng ký tài khoản (Sign In)</a></li>
     <li><a href="/check-age">Kiểm tra độ tuổi (Middleware)</a></li>
 </ul>
